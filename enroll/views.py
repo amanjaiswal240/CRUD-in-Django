dbad0c5bb68eecb874aa8a704e2f288e686105a2
Aman <jayswalaman240@gmail.com>
2021-06-19 19:16:31 +0530

    Initial Commit

---
+++ enroll/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, HttpResponseRedirect
+from .forms import StudentRegistration
+from .models import User
+
+# Create your views here.
+def index(request):
+    if request.method == "POST":
+        fm=StudentRegistration(request.POST)
+        if fm.is_valid():
+            nm=fm.cleaned_data['name']
+            em=fm.cleaned_data['email']
+            pw=fm.cleaned_data['password']
+            reg=User(name=nm,email=em,password=pw)
+            reg.save()
+            fm=StudentRegistration()
+    else:
+        fm=StudentRegistration()
+    stud=User.objects.all()
+    return render(request,'enroll/index.html',{'form':fm, 'student':stud})
+
+def delete_data(request,id):
+    if request.method=='POST':
+        pi=User.objects.get(pk=id)
+        pi.delete()
+        return HttpResponseRedirect('/enroll')
+
+def update_data(request,id):
+    if request.method=='POST':
+        pi=User.objects.get(pk=id)
+        fm=StudentRegistration(request.POST, instance=pi)
+        if fm.is_valid():
+            fm.save()
+            return HttpResponseRedirect('/enroll')
+    else:
+        pi=User.objects.get(pk=id)
+        fm=StudentRegistration(instance=pi)
+    return render(request, 'enroll/updatestudent.html',{'form':fm})
